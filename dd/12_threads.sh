22354a2b84c07aa0d70dbdb93251d28d520eb2f7
shravyaGaddam <shravya.g90@gmai.com>
2015-12-19 16:58:17 +0000

    updated ceph-scripts

---
+++ dd/12_threads.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo dd if=/dev/zero of=/home/ubuntu/deleteme bs=1G count=1 oflag=direct
+sudo dd if=/home/ubuntu/deleteme of=/dev/null bs=1G count=1 iflag=direct 2> /home/ubuntu/dd_read_12threads.txt
+sudo rm /home/ubuntu/deleteme
